2ad9b24ef61e3f5fea9c1e61e6293a72b5ec7272
HessahSWE <hissahalhussain@gmail.com>
2022-12-05 22:48:04 +0300

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AdminComponent } from './admin/admin.component';
+import { UserComponent } from './user/user.component';
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
+import { ForbiddenComponent } from './forbidden/forbidden.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AdminComponent
+    AdminComponent,
+    UserComponent,
+    LoginComponent,
+    HeaderComponent,
+    ForbiddenComponent
   ],
   imports: [
     BrowserModule
