9a8bf00990af9003518665b4d1fda91cf34b741c
HessahSWE <hissahalhussain@gmail.com>
2022-12-07 02:44:17 +0300

    update

--- src/app/_service/user.service.ts
+++ src/app/_service/user.service.ts
@@ -1,9 +1,13 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-
-  constructor() { }
+  PathOfAPI = 'http://localhost:9090';
+  constructor(private httpClient: HttpClient) { }
+  public login(loginData) {
+    return this.httpClient.post(this.PathOfAPI + "/authenticate", loginData);
+  }
 }
